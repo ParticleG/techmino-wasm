d35b129ed01d6a8211fd54f7195a506de0f8a58b
ParticleG <particle_g@outlook.com>
2024-11-02 18:19:32 +0800

    -

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"941ac1e3-bfb3-4cd6-b722-0635888d4828","remote_package_size":67868449,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":67868449,"audio":false}]});
+  loadPackage({"package_uuid":"20cd01a7-1744-4033-955a-ff112544f56a","remote_package_size":67868449,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":67868449,"audio":false}]});
 
 })();
